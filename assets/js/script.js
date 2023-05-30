d6246d1379ab562a4e624d2fe9ac57107647f145
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-05-30 07:42:09 -0700

    added js using jquery and dayjs to have a functioning date and 24 hour clock, added an event listener to save the data in the textarea element to local storage based on the hour and display them on the page using the same hour id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveButton = $(".saveBtn");
+
+var timeCounter = function () {
+  var currentTime = dayjs().format("MMM DD, YYYY HH:mm:ss a");
+  $("#currentDay").text(currentTime);
+};
+
+$(timeCounter());
+
+$(setInterval(timeCounter, 1000));
+
+$(
+  saveButton.on("click", function (event) {
+    var hourId = "#" + this.parentElement.id;
+    var plans = $(hourId).children().eq(1).val();
+    localStorage.setItem(hourId, plans);
+  })
+);
+
+for (var i = 0; i < localStorage.length; i++) {
+  var storageKey = localStorage.key(i).split("#")[1];
+  var displayAgenda = localStorage.getItem("#" + storageKey);
+  $("#" + storageKey)
+    .children()
+    .eq(1)
+    .text(displayAgenda);
+}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,7 +36,6 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
